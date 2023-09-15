6bb564b1bdabdaf9a53cb3894389559f1fc07760
OkruzhkoVS <OkruzhkoVS@310D-7>
2023-09-15 13:38:28 +0700

    Добавьте файлы проекта.

---
+++ CopyOfQueue/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CopyOfQueue
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            foreach (int item in queue.GetValue())
+            {
+                Console.WriteLine(item);
+            }
+
+            queue.Dequeue();
+
+            Console.WriteLine();
+
+            foreach (int item in queue.GetValue())
+            {
+                Console.WriteLine(item);
+            }
+
+            var copy = new Queue<int>(queue);
+
+            Console.WriteLine();
+
+            foreach (int item in copy.GetValue())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CopyOfQueue/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyOfQueue
+{
+    class Queue<T>
+    {
+        private List<T> list;
+        private int Count { get; set; }
+
+        public Queue()
+        { 
+            list = new List<T>();
+            Count = 0;
+        }
+
+        public Queue(Queue<T> obj) //Конструктор копирования
+        {
+            list = obj.list;
+            Count = obj.Count;
+        }
+
+        public void Enqueue(T item)
+        {
+            list.Add(item);
+            Count++;
+        }
+
+        public void Dequeue()
+        {
+            if (list.Count > 0)
+            {
+                list.Remove(list.First());
+                Count--;
+            }
+            else throw new NullReferenceException("Очередь пуста");
+        }
+
+        public List<T> GetValue()
+        {
+            return list;
+        }
+    }
+}
